b69c0e02db1724c8c8a8ef28a7c93cc287ea6cb6
Estefania <nialupo87@gmail.com>
2020-12-30 17:58:53 -0500

    created models folders with index and workouts files

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-// const db = require("./models");
+const db = require("./models");
 
 const app = express();
 
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", { 
 useNewUrlParser: true,
 useUnifiedTopology: true,
 useFindAndModify: false,
