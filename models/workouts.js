e29fa62eacc1f95cb5ed940ad611870b1ef010be
Estefania <nialupo87@gmail.com>
2021-01-02 22:13:49 -0500

    added schema to workout.js file

--- models/workouts.js
+++ models/workouts.js
@@ -2,6 +2,35 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutsSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    excercises: [{
+        type: {
+            type: String,
+            trim: true,
+            require: "Type is required"
+        },
+        name: {
+            type: String,
+            trim: true,
+            require: "Name is required"
+        },
+        duration: {
+            type: Number
+        },
+        weight: {
+            type: Number
+        },
+        reps: {
+          type: Number  
+        },
+        sets: {
+            type: Number
+        }
+
+    }]
  
 });
 
