d1c3fd8964752043f3efce098f9044d7fc87bf2e
Estefania <nialupo87@gmail.com>
2021-01-03 13:59:21 -0500

    added view routes and fixed error with models

---
+++ controller/workoutRoutes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const db = require("../models");
+const path = require("path");
+
+const router = express.Router();
+
+// VIEW ROUTES
+
+router.get("/", (req,res)=> {
+    res.sendFile(path.join(__dirname + "/public/index.html"))
+});
+
+router.get("/stats", (req,res)=> {
+    res.sendFile(path.join(__dirname + "/public/stats.html"))
+});
+
+router.get("/exercise", (req,res)=> {
+    res.sendFile(path.join(__dirname + "/public/exercise.html"))
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const path = require("path");
 
 const PORT = process.env.PORT || 3002;
 
@@ -21,6 +20,10 @@ useNewUrlParser: true,
 useUnifiedTopology: true,
 useFindAndModify: false,
 useCreateIndex: true});
+
+const routes = require("./controller/workoutRoutes.js");
+
+app.use(routes);
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
