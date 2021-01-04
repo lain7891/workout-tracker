b4a5d555a0dfe95c53c112ad66fa9f7563bcbe5e
Estefania <nialupo87@gmail.com>
2021-01-03 19:24:46 -0500

    took public and seed folders outside of the develop folder

--- controller/workoutRoutes.js
+++ controller/workoutRoutes.js
@@ -11,20 +11,28 @@ const router = express.Router();
 // VIEW ROUTES
 
 router.get("/index", (req,res)=> {
-    res.sendFile(path.join(__dirname, "/public/index.html"))
+    res.sendFile(path.join(__dirname, "./public/index.html"))
 });
 
 router.get("/stats", (req,res)=> {
-    res.sendFile(path.join(__dirname, "/public/stats.html"))
+    res.sendFile(path.join(__dirname, "./public/stats.html"))
 });
 
 router.get("/exercise", (req,res)=> {
-    res.sendFile(path.join(__dirname, "/public/exercise.html"))
+    res.sendFile(path.join(__dirname, "./public/exercise.html"))
 });
 
-router.get("/api/workout", (req, res)=>{
-    res.json(workouts)
-})
+router.get("/api/workouts", (req, res)=>{
+    db.Workout.find().then((foundWorkout) => {
+        res.json(foundWorkout);
+        console.log(foundWorkout);
+    })})
+
+
+
+
+
+
 
 
 
--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("/public"));
+app.use(express.static("./public"));
 
 
 // app.get('/', function(req, res){
